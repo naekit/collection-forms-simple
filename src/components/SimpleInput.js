6f3fb3dde5912a232041989a0e530827edbc71f3
naekit <mykyta.onishchuck@code.berlin>
2023-01-12 11:10:14 -0500

    validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,53 @@
-const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
-};
+import { useRef, useState } from "react"
 
-export default SimpleInput;
+const SimpleInput = (props) => {
+	const nameRef = useRef()
+	const formRef = useRef()
+	const [name, setName] = useState("")
+	const [isValid, setIsValid] = useState(true)
+
+	const nameHandler = (e) => {
+		e.preventDefault()
+		setIsValid(true)
+		setName(e.target.value)
+	}
+
+	const formHandler = (e) => {
+		e.preventDefault()
+		const enteredName = nameRef.current.value
+		// formRef.current.reset()
+		if (name.trim() === "") {
+			setIsValid(false)
+			return
+		}
+		setIsValid(true)
+		setName("")
+		console.log(name)
+		console.log(enteredName)
+	}
+
+	const formClasses = isValid ? "form-control" : "form-control invalid"
+
+	return (
+		<form ref={formRef} onSubmit={formHandler}>
+			<div className={formClasses}>
+				<label htmlFor="name">Your Name</label>
+				<input
+					value={name}
+					ref={nameRef}
+					type="text"
+					id="name"
+					onChange={nameHandler}
+				/>
+				{!isValid && (
+					<p className="error-text">Name must not be empty</p>
+				)}
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	)
+}
+
+export default SimpleInput
