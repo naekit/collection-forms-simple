bf013bd85b89ce047febe7ebfbb15bba615d18e5
naekit <mykyta.onishchuck@code.berlin>
2023-01-14 12:37:36 -0500

    reducer hook for custom hook

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -1,27 +1,43 @@
-import { useState, useReducer } from "react"
+import { useReducer } from "react"
+
+const initialState = {
+	value: "",
+	isTouched: false,
+}
+
+const inputReducer = (state, action) => {
+	if (action.type === "INPUT") {
+		return { value: action.value, isTouched: state.isTouched }
+	}
+	if (action.type === "BLUR") {
+		return { value: state.value, isTouched: true }
+	}
+	if (action.type === "RESET") {
+		return initialState
+	}
+	return initialState
+}
 
 const useInput = (validateVal) => {
-	const [value, setValue] = useState("")
-	const [isTouched, setIsTouched] = useState("")
+	const [inputState, dispatch] = useReducer(inputReducer, initialState)
 
-	const valueIsValid = validateVal(value)
-	const hasError = !valueIsValid && isTouched
+	const valueIsValid = validateVal(inputState.value)
+	const hasError = !valueIsValid && inputState.isTouched
 
 	const valueHandler = (e) => {
-		setValue(e.target.value)
+		dispatch({ type: "INPUT", value: e.target.value })
 	}
 
 	const blurHandler = (e) => {
-		setIsTouched(true)
+		dispatch({ type: "BLUR" })
 	}
 
 	const reset = () => {
-		setValue("")
-		setIsTouched(false)
+		dispatch({ type: "RESET" })
 	}
 
 	return {
-		value,
+		value: inputState.value,
 		hasError,
 		valueHandler,
 		blurHandler,
